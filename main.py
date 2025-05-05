34311e5da0beee0880f130e87f555a0263ba2db0
i314q159 <i314q159@outlook.com>
2025-05-05 14:27:41 +0800

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
+import requests
 
-# 获取指定文件夹下的所有文件路径
 def get_all_file_paths(folder_path):
     file_paths = []
     for root, _, files in os.walk(folder_path):
@@ -8,3 +8,22 @@ def get_all_file_paths(folder_path):
             file_paths.append(os.path.join(root, file))
     return file_paths
 
+
+def get_response(url, picture_path):
+    data = {
+        "model": "llava",
+        "prompt": "图片里面是什么?",
+        "images": [picture_path],
+    }
+
+    response = requests.post(url, json=data)
+    print("图片路径: ", picture_path)
+    print("状态码: ", response.status_code)
+
+
+url = "http://localhost:11434/api/generate"
+folder_path = "d:/wallpaper_d/"
+file_paths = get_all_file_paths(folder_path)
+
+for file_path in file_paths:
+    get_response(url, file_path)
--- test.ps1
+++
@@ -1,5 +0,0 @@
-curl http://localhost:11434/api/generate -d '{
-  "model": "llava",
-  "prompt":"What is in this picture?",
-  "images": ["d:/wallpaper_d/0ccyem.jpg"],
-}'
